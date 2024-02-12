bdb2d35b26c32ee7716fe29072aff72a8dd86184
Cenk Kıran <cenk33m@gmail.com>
2024-02-12 20:05:02 +0300

    Detail page was written

--- lib/ui/views/detail_page.dart
+++ lib/ui/views/detail_page.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../../data/entity/note.dart';
+
 class DetailPage extends StatefulWidget {
-  const DetailPage({super.key});
+  Note note;
+  DetailPage({required this.note});
 
   @override
   State<DetailPage> createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
+  var tfTitle = TextEditingController();
+  var tfContent = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tfTitle.text = widget.note.title;
+    tfContent.text = widget.note.content;
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Note"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(controller: tfTitle,decoration: InputDecoration(
+                labelText: tfTitle.text.isEmpty ? "" : "${widget.note.title}"
+          )),
+            TextField(controller: tfContent,decoration: InputDecoration(
+                labelText: tfContent.text.isEmpty ? "" : "${widget.note.content}"
+            )),
+            ElevatedButton(onPressed: (){
+              //Update button
+            }, child: const Text("Update"))
+
+          ],
+        ),
+      ),
+    );
   }
 }
