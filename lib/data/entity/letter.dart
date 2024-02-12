150c4c3d0bc79c5e8d43aa38aedfa4fc76a43ea7
Cenk Kıran <cenk33m@gmail.com>
2024-02-12 20:02:39 +0300

    letter was change note

--- lib/data/entity/letter.dart
+++
@@ -1,7 +0,0 @@
-class Letter{
-  int letter_id;
-  String title;
-  String content;
-
-  Letter({required this.letter_id,required this.title,required this.content});
-}
---
+++ lib/data/entity/note.dart
@@ -0,0 +1,8 @@
+class Note{
+  int letter_id;
+  String title;
+  String content;
+
+  Note({required this.letter_id,required this.title,required this.content});
+
+}
